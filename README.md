2a52cd56d0d0944dd2956103f1775c703c072750
Denghejun <hjdeng@CNhjdeng.local>
2018-08-02 22:28:50 +0800

    [Hejun] add profile

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-可以用 Zuul 构建网关
+1.可以用 Zuul 构建网关
+2.mvn spring-boot:run -Dspring-boot.run.profiles=dev
--- bff/src/main/java/rest/service/architecture/bff/app/UserApi.java
+++ bff/src/main/java/rest/service/architecture/bff/app/UserApi.java
@@ -1,5 +1,6 @@
 package rest.service.architecture.bff.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import user.model.User;
 
@@ -9,6 +10,9 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/app/user")
 public class UserApi {
+
+    @Value("${spring.datasource.url}")
+    private String text;
 
     @PostMapping
     public List<User> addUsers(List<User> users) {
@@ -23,7 +27,7 @@ public class UserApi {
     public User getUser(@PathVariable("userId") String userId) {
         User u = new User();
 
-        u.setName("you");
+        u.setName(this.text);
         return u;
     }
 }
