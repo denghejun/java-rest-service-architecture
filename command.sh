dd8a42258f289a2992b663f4cfa1a8d3492bcba4
Denghejun <hjdeng@CNhjdeng.local>
2019-08-13 15:07:26 +0800

    Revert "test"
    This reverts commit 0a015b68

--- command.sh
+++ command.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
-
-for file in $(git diff --name-only --cached \*.java)
+for file in $(git diff --name-only \*.java)
 do
   fileName=$(basename "$file")
   git show ":$file" | mvn checkstyle:check -Dcheckstyle.includes=**\\/"$fileName" # we only want to lint the staged changes, not any un-staged changes
